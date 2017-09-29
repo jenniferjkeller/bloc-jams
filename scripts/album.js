22e2814598f99b7b0383b05a1f3017f07fb27a1b
Jennifer Keller <jennifer.keller1125@gmail.com>
2017-09-29 13:16:46 -0700

    assignment-24-completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,20 @@ var albumPicasso = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+ var albumEndOfSummer = {
+     title: 'Backyard Barbeque',
+     artist: 'Everglades',
+     label: 'subpop',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: '80/20 rule', duration: '2:08' },
+         { title: 'Reap what you sow', duration: '3:01' },
+         { title: 'Grillin and chillin', duration: '4:10'},
+         { title: 'Don\'t go', duration: '3:14' },
+         { title: 'Fall back', duration: '2:22'}
+     ]
+ };
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -39,15 +53,16 @@ var albumPicasso = {
 
      return template;
  };
+
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
  var setCurrentAlbum = function(album) {
     // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
-    // #2
+  
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -62,6 +77,18 @@ var albumPicasso = {
     }
 };
 
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    var index = 1;
+    var albums = [albumPicasso, albumMarconi, albumEndOfSummer]
+    albumImage.addEventListener("click", function(event) {
+      setCurrentAlbum(albums[index]);
+      index++;
+        if (index == albums.length) {
+          index = 0;
+        };
+
+      });
 };
